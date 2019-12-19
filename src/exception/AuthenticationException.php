90235218ff72d5847755c9fc5c63c8bd98bb1e3e
china_wangyu <china_wangyu@aliyun.com>
2019-12-19 16:59:26 +0800

    初始化扩展

---
+++ src/exception/AuthenticationException.php
@@ -0,0 +1,12 @@
+<?php
+/** Created by 嗝嗝<china_wangyu@aliyun.com>. Date: 2019-11-20  */
+
+namespace paa\annotation\exception;
+
+
+class AuthenticationException extends \Exception
+{
+    protected $message = '鉴权错误';
+    protected $code = 400;
+    protected $error_code = 1001;
+}
---
+++ src/handler/Jwt.php
@@ -0,0 +1,20 @@
+<?php
+declare (strict_types = 1);
+
+namespace paa\annotation\handler;
+
+use Doctrine\Common\Annotations\Annotation;
+use think\annotation\handler\Handler;
+
+final class Jwt extends Handler
+{
+
+    public function func(\ReflectionMethod $refMethod, Annotation $annotation, \think\route\RuleItem &$rule)
+    {
+        if ($this->isCurrentMethod($refMethod,$rule)){
+            \app\common\authorize\Jwt::check();
+        }
+    }
+
+    
+}
