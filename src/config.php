abc99772fa88e9377b19654aa73a0cfa28085d65
china_wangyu <china_wangyu@aliyun.com>
2020-03-17 22:42:43 +0800

    '更新底层依赖'

--- src/config.php
+++ src/config.php
@@ -9,11 +9,18 @@ return [
         'enable'      => true,
         'controllers' => [],
     ],
+    'model'  => [
+        'enable' => true,
+    ],
     'ignore' => [],
     'custom' => [
+        'enable'      => true,
         # 格式：注解类 => 注解操作类
-        \paa\annotation\Param::class => \paa\annotation\handler\Param::class, # 单个参数验证器
-        \paa\annotation\Jwt::class => \paa\annotation\handler\Jwt::class, # JWT注解验证器
-        \paa\annotation\Doc::class => \paa\annotation\handler\Doc::class, # 文档管理器
-    ]
+        'namespaces' => [
+            # 格式：注解类 => 注解操作类
+            \paa\annotation\Param::class => \paa\annotation\handler\Param::class, # 单个参数验证器
+            \paa\annotation\Jwt::class => \paa\annotation\handler\Jwt::class, # JWT注解验证器
+            \paa\annotation\Doc::class => \paa\annotation\handler\Doc::class, # 文档管理器
+        ],
+    ],
 ];
